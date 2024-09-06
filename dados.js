b3a287b5271192b310c9d671ab88a61524d6d1cf
AliceC-Abreu <aliceabmonteiro@gmail.com>
2024-09-06 17:53:40 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,14 @@
+    let dados = [
+        {
+            titulo: "A Ordem",
+            descrição: "A Ordo Realitas é uma organização secreta que atua nas sombras, dedicada à proteção da nossa realidade. Composta por indivíduos com um profundo conhecimento do oculto, a Ordem combate ameaças sobrenaturais que buscam desestabilizar o nosso mundo. Seus agentes, muitas vezes vivendo uma dupla vida, investigam e neutralizam entidades malignas, fenômenos inexplicáveis e portais para outras dimensões. A Ordo Realitas é a linha de frente na batalha contra o paranormal, garantindo que o equilíbrio entre o nosso mundo e o Outro Lado seja mantido.",
+            link: "https://ordemparanormal.fandom.com/wiki/Ordo_Realitas?",
+            tags: "Veríssimo Realidade"
+        },
+        {
+            titulo: "O Outro Lado",
+            descrição: "O Outro Lado é um plano existencial paralelo ao nosso, habitado por entidades e forças sobrenaturais. É uma dimensão caótica e instável, onde as leis da física não se aplicam e o perigo espreita em cada esquina. Os Elementos do Outro Lado são uma das principais fundações do Ocultismo. Esses que podem ser divididos em cinco principais: Sangue, Morte, Conhecimento, Energia e Medo. Cada um desses elementos possuem propriedades únicas e são responsáveis por definir as características das materializações do Outro Lado na Realidade.",
+            link: "https://ordemparanormal.fandom.com/wiki/Elementos_do_Outro_Lado",
+            tags: "Paranormal Sobrenatural"
+        }
+    ];
